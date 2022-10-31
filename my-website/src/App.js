75a680e81a3f90514cef5f16b9801086205c3522
Zach Stange <zstange@wisc.edu>
2022-10-30 20:26:01 -0500

    Welcome message updates

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -7,16 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my personal website! I'm currently working on developing it. Please check back later for updates.
         </p>
+        <p> 
+        In the meantime, be sure to checkout my profile, and connect with me, on&nbsp;
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/zachary-stange/"
+          target = "_blank"
         >
-          Learn React
+          LinkedIn
         </a>
+        </p>
       </header>
     </div>
   );
